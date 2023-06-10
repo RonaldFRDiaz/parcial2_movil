ddf9c0f22b51b726c14a6d027d37a7f32df47dc7
RonaldRivas <00131321@uca.edu.sv>
2023-06-10 01:06:26 -0600

    cambio_pero_con_fallos

--- app/src/main/java/com/example/parcial2_00131321_ronald_rivas/ui/tracker/Listas/recyclerview/TrackerRecyclerViewHolder.kt
+++ app/src/main/java/com/example/parcial2_00131321_ronald_rivas/ui/tracker/Listas/recyclerview/TrackerRecyclerViewHolder.kt
@@ -4,7 +4,7 @@ package com.example.parcial2_00131321_ronald_rivas.ui.tracker.Listas.recyclervie
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parcial2_00131321_ronald_rivas.data.model.trackermodel
 import com.example.parcial2_00131321_ronald_rivas.databinding.TrackerItemBinding
-
+//hola
 class TrackerRecyclerViewHolder(private val binding: TrackerItemBinding): RecyclerView.ViewHolder(binding.root) {
     fun bind(tracker: trackermodel, clickListener: (trackermodel) -> Unit) {
         binding.nameTextView.text = tracker.name
